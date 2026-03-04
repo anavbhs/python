8e8833f11fe865e0dba22b005491eb575773ac41
anavbhs <anavitoriabsant0s3@gmail.com>
2026-03-04 12:17:56 +0000

    oi

--- projects.py
+++ projects.py
@@ -1,3 +1,8 @@
-str = "nome_usuario "
-int = senha
-eh_verificado = bool
+pontuacao = 85
+
+if pontuacao >= 90:
+    pass
+elif pontuacao >= 70:
+    print("bom desempenho")
+else:
+    print("precisa melhorar")

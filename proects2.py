92ed031e87e9eb4582074e8cf430b0f3aec4c6d3
anavbhs <anavitoriabsant0s3@gmail.com>
2026-03-18 14:29:01 +0000

    oi

--- proects2.py
+++ proects2.py
@@ -1,1 +1,21 @@
-print("")
+print("Passageiros do voo:\n ")
+passageiros = ['Ana','Bruno','Carlos','Daniela','Eduardo']
+
+for voo in passageiros:
+    
+    if voo == 'Carlos':
+        print(voo,"perdeu o check-in!")
+        continue
+    
+    print(voo,"realizou o check-in!")
+
+for embarque in passageiros:
+    if embarque == "Daniela":
+        break 
+    if embarque == "Carlos":
+        print(embarque,"perdeu o embarque")
+        continue
+    else: 
+        print("Embarque encerrado!")
+ 
+        print(embarque,"pode embarcar")

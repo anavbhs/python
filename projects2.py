666b592591116ca3963e342dbbba48f4d53bef08
anaviboahoras <anavboahorasantos@gmail.com>
2026-03-24 21:30:12 -0300

    fin

--- projects2.py
+++ projects2.py
@@ -6,17 +6,23 @@ while novo_item_do_pedido == 'S':
     if valor_item == 0:
         print(f"Pedido de {nome} cancelado")
         break
+
     if valor_item < 0:
         print("Valor inválido, tente novamente")
-    continue
+        continue
+    
     if valor_item > 50:
         print("Item premium")
+
     elif valor_item > 20 and valor_item <= 50:
         print("Item normal")
+
     else:
         print("Item econômico")
+    
     total_pedido += valor_item
     novo_item_do_pedido = input(f"{nome}, deseja registrar um novo item no pedido? (S/N): ").upper()
-    else:
+
+else:
     print(f'Valor total do pedido de {nome}: R${total_pedido:.2f}')
     print("Finalizando...")
